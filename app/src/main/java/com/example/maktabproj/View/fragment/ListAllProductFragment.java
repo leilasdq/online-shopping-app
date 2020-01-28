f6a49ad4c07c08e4d3dd2decf64c3805a92faaa1
leila <l.sedighi.2015@gmail.com>
2020-01-28 10:09:08 +0330

    Add searchView to search products in ListAllProductsFragment.

--- app/src/main/java/com/example/maktabproj/View/fragment/ListAllProductFragment.java
+++ app/src/main/java/com/example/maktabproj/View/fragment/ListAllProductFragment.java
@@ -1,8 +1,12 @@
 package com.example.maktabproj.View.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
@@ -10,9 +14,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.maktabproj.View.activity.SearchResultActivity;
 import com.example.maktabproj.View.adapter.recycler.EndlessRecyclerView;
 import com.example.maktabproj.View.adapter.recycler.recyclerViewAdapter.ListAllProductAdapter;
 import com.example.maktabproj.Model.Response;
@@ -37,6 +46,7 @@ public class ListAllProductFragment extends Fragment {
     private ListAllProductAdapter adapter;
     private FragmentListAllProductBinding mBinding;
     private ListAllProductsViewModel mViewModel;
+    private SearchView searchView;
 
     public static ListAllProductFragment newInstance(String productType) {
 
@@ -69,8 +79,8 @@ public class ListAllProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_list_all_product, container, false);
-        setupRecycle();
         setupToolbar();
+        setupRecycle();
 
         scrollListener = new EndlessRecyclerView(manager) {
             @Override
@@ -110,6 +120,35 @@ public class ListAllProductFragment extends Fragment {
         } else if (type.equalsIgnoreCase("rated")) {
             mBinding.allListsToolbar.setTitle(getString(R.string.all_rated));
         }
-        mBinding.allListsToolbar.setTitleTextColor(getActivity().getResources().getColor(android.R.color.white));
-    }
+        mBinding.allListsToolbar.setTitleTextColor(getActivity().getResources().getColor(android.R.color.black));
+
+        mBinding.allListsToolbar.inflateMenu(R.menu.tab_menu);
+        Menu menu = mBinding.allListsToolbar.getMenu();
+        MenuItem searchMenuItem = menu.findItem(R.id.app_bar_search);
+        searchView = (SearchView) searchMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+//                searchView.setIconified(true);
+                Toast.makeText(getContext(), query, Toast.LENGTH_SHORT).show();
+                Intent intent = SearchResultActivity.newIntent(getActivity(), query);
+                startActivity(intent);
+                searchView.onActionViewCollapsed();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        mBinding.allListsToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        }); }
+
+
 }
